3c099d5767ab5749ec9c2eb3b4318a0d4e918db5
Ryan Brott <rcbrott@gmail.com>
2023-11-26 13:55:10 -0800

    Add downsampled writer

--- RoadRunner/src/main/java/com/acmerobotics/roadrunner/ftc/FlightRecorder.kt
+++ RoadRunner/src/main/java/com/acmerobotics/roadrunner/ftc/FlightRecorder.kt
@@ -105,3 +105,14 @@ object FlightRecorder : OpModeManagerNotifier.Notifications {
         }
     }
 }
+
+class DownsampledWriter(val channel: String, val maxPeriod: Long) {
+    private var nextWriteTimestamp = 0L
+    fun write(msg: Any) {
+        val now = System.nanoTime()
+        if (now >= nextWriteTimestamp) {
+            nextWriteTimestamp = (now / maxPeriod + 1) * maxPeriod
+            FlightRecorder.write(channel, msg)
+        }
+    }
+}
