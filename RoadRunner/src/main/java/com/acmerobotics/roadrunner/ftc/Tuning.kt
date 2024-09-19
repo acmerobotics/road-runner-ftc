592d047a4dd6d65c769c8643d16f90dbb9de409c
Ryan Brott <rcbrott@gmail.com>
2024-09-18 21:30:39 -0700

    Get angular velocity in degrees
    Workaround for https://github.com/FIRST-Tech-Challenge/FtcRobotController/issues/1070

--- RoadRunner/src/main/java/com/acmerobotics/roadrunner/ftc/Tuning.kt
+++ RoadRunner/src/main/java/com/acmerobotics/roadrunner/ftc/Tuning.kt
@@ -256,16 +256,17 @@ class AngularRampLogger(val dvf: DriveViewFactory) : LinearOpMode() {
             }
 
             t.addSplit()
-            val av = view.imu.get().getRobotAngularVelocity(AngleUnit.RADIANS)
+            // Use degrees here to work around https://github.com/FIRST-Tech-Challenge/FtcRobotController/issues/1070
+            val av = view.imu.get().getRobotAngularVelocity(AngleUnit.DEGREES)
             val time = t.addSplit()
 
             data.angVels[0].times.add(time)
             data.angVels[1].times.add(time)
             data.angVels[2].times.add(time)
 
-            data.angVels[0].values.add(av.xRotationRate.toDouble())
-            data.angVels[1].values.add(av.yRotationRate.toDouble())
-            data.angVels[2].values.add(av.zRotationRate.toDouble())
+            data.angVels[0].values.add(Math.toRadians(av.xRotationRate.toDouble()))
+            data.angVels[1].values.add(Math.toRadians(av.yRotationRate.toDouble()))
+            data.angVels[2].values.add(Math.toRadians(av.zRotationRate.toDouble()))
         }
 
         for (m in view.motors) {
